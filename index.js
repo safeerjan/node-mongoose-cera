e7249ac548c10e283f82160eaca2540de9c4229b
safeerjan <Jan.Safeer@vistajet.com>
2019-04-30 15:34:17 +0500

    Mongoose Part 1

---
+++ index.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const Dishes = require('./models/dishes');
+
+const url = 'mongodb://localhost:27017/conFusion';
+const connect = mongoose.connect(url);
+
+connect.then((db) => {
+
+  console.log('Connected correctly to server');
+
+  var newDish = Dishes({
+    name: 'Uthappizza',
+    description: 'test'
+  });
+
+  newDish.save()
+    .then((dish) => {
+      console.log(dish);
+
+      return Dishes.find({});
+    })
+    .then((dishes) => {
+      console.log(dishes);
+
+      return Dishes.remove({});
+    })
+    .then((dish) => {
+      console.log(dish);
+      return Dishes.find({});
+    })
+    .then(() => {
+      return mongoose.connection.close();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+});
